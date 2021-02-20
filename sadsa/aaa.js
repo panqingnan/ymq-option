3912a1818c8ef2c48b819123879e277b35613994
panqingnan <50012625+panqingnan@users.noreply.github.com>
2021-02-20 13:20:41 +0800

    Update aaa.js

--- sadsa/aaa.js
+++ sadsa/aaa.js
@@ -2,4 +2,4 @@ ar form_box = '#ymq-option-form';
 	var ymq_option_title = 'ymq-option-title';
 	var basImgUrl = 'http://ymq-shopify.oss-us-east-1.aliyuncs.com/';
 	var key_prefix = 'ymq';
-	var please = 'please';
+	var please = 'please111';
