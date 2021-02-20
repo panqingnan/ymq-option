23d6c099fa851cb219d2e950ddd175fb1b4d804f
panqingnan <50012625+panqingnan@users.noreply.github.com>
2021-02-20 13:18:38 +0800

    Create aaa.js

---
+++ sadsa/aaa.js
@@ -0,0 +1,5 @@
+ar form_box = '#ymq-option-form';
+	var ymq_option_title = 'ymq-option-title';
+	var basImgUrl = 'http://ymq-shopify.oss-us-east-1.aliyuncs.com/';
+	var key_prefix = 'ymq';
+	var please = 'please';
